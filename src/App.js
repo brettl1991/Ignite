3f0e911be0d9ef49c8d380f68f656063b90d4241
brettl1991 <brettl1991@gmail.com>
2022-08-22 11:57:16 +0100

    Add global styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 
 import Home from "./pages/Home";
 
+import GlobalStyles from "./components/GlobalStyles";
+
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Home />
     </div>
   );

ca0afb0bf93f6d4f3dc1d1e5b02835c6aefb2131
brettl1991 <brettl1991@gmail.com>
2022-08-25 20:01:40 +0100

    Add and style nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 //Components and pages
-
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
 
+//Styles
 import GlobalStyles from "./components/GlobalStyles";
 
+//Router
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
+      <Nav />
 
       <Routes>
         <Route exact path="/game/:id" element={<Home />} />
